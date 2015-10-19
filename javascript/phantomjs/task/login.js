a66421958b5e214e471f80783460d609acc8cf3c
hungle <lecaoquochung@gmail.com>
2015-10-19 17:06:44 +0900

    Task login emulation

---
+++ javascript/phantomjs/task/login.js
@@ -0,0 +1,25 @@
+console.log("got here");
+var page = require('webpage').create();
+
+page.onConsoleMessage = function(msg) {
+    console.log(msg);
+};
+
+page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36';
+page.viewportSize = {width: 1280,height: 768};
+
+page.open("http://facebook.com", function(status) {
+    if ( status === "success" ) {
+        page.evaluate(function() {
+              document.querySelector("input[name='email']").value = "email";
+              document.querySelector("input[name='pass']").value = "pass";
+              document.querySelector("#login_form").submit();
+
+              console.log("Login submitted!");
+        });
+        window.setTimeout(function () {
+          page.render('colorwheel.png');
+          phantom.exit();
+        }, 5000);
+   }
+});
