3cf72b97bc56a7f087c90c19682f5d89deffc58f
hungle <lecaoquochung@gmail.com>
2015-10-26 14:03:20 +0900

    jquery bind: block right click

--- javascript/phantomjs/task/login.js
+++ javascript/phantomjs/task/login.js
@@ -11,8 +11,8 @@ page.viewportSize = {width: 1280,height: 768};
 page.open("http://facebook.com", function(status) {
     if ( status === "success" ) {
         page.evaluate(function() {
-              document.querySelector("input[name='email']").value = "email";
-              document.querySelector("input[name='pass']").value = "pass";
+              document.querySelector("input[name='email']").value = "test@ittvn.com";
+              document.querySelector("input[name='pass']").value = "ittvn123";
               document.querySelector("#login_form").submit();
 
               console.log("Login submitted!");
