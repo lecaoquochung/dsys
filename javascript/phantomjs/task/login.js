4c0d08be3bfd81331e8684ebe3be12d20ff7e5eb
Hung Le <lecaoquochung@gmail.com>
2015-11-15 15:03:37 +0900

    phantomcs

--- javascript/phantomjs/task/login.js
+++ javascript/phantomjs/task/login.js
@@ -8,11 +8,11 @@ page.onConsoleMessage = function(msg) {
 page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36';
 page.viewportSize = {width: 1280,height: 768};
 
-page.open("http://facebook.com", function(status) {
+page.open("http://www.facebook.com", function(status) {
     if ( status === "success" ) {
         page.evaluate(function() {
-              document.querySelector("input[name='email']").value = "email";
-              document.querySelector("input[name='pass']").value = "pass";
+              document.querySelector("input[name='email']").value = "test@ittvn.com";
+              document.querySelector("input[name='pass']").value = "ittvn123";
               document.querySelector("#login_form").submit();
 
               console.log("Login submitted!");
